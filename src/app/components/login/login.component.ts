7a7a76e6d58d2f1d561a9d19a0c958ff7140111e
Markjarnaiz <Sc.markj0307@gmail.com>
2025-07-16 22:19:13 +0800

    UI ENHANCEMENT: use snackbar on login and enhance the modal of tracking page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-login',
@@ -15,7 +17,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
@@ -28,14 +31,31 @@ export class LoginComponent {
       this.authService.login(this.loginForm.value).subscribe({
         next: (res) => {
           this.authService.storeToken(res.token);
+          this.snackBar.open('Login successful!', 'Close', {
+            duration: 3000,
+            verticalPosition: 'top',
+            panelClass: 'snackbar-success'
+          });
           this.router.navigate(['/dashboard']);
         },
         error: (err) => {
           this.errorMsg = err.error.message || 'Login failed';
+          this.snackBar.open(this.errorMsg, 'Close', {
+            duration: 3000,
+            verticalPosition: 'top',
+            panelClass: 'snackbar-error'
+          });
         }
+      });
+    } else {
+      this.snackBar.open('Username and password are required.', 'Close', {
+        duration: 3000,
+        verticalPosition: 'top',
+        panelClass: 'snackbar-error'
       });
     }
   }
+
 
    navigateToHome() {
     this.router.navigateByUrl('/home');
